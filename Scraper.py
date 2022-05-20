41c38eada82bc1d49a749de83f5cd4c222bfd514
Bearre <97527006+Bearre@users.noreply.github.com>
2022-05-20 15:22:23 +0300

    Create Scraper.py
    Парсим сайт https://coinmarketcap.com/ru
    Выводим актуальную информацию по криптовалютам

---
+++ Scraper.py
@@ -0,0 +1,84 @@
+import time
+import requests
+import random
+from bs4 import BeautifulSoup as BS
+
+
+UserAgents = [
+    'Mozilla/5.0 (Linux; U) Opera 6.02 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows NT 4.0) Opera 6.02 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows 95) Opera 6.02 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows 95) Opera 6.02 [de]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows 2000) Opera 6.02 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Linux 2.4.20-686 i686) Opera 6.02 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Linux 2.4.18-4GB i686) Opera 6.02 [en]',
+    'Opera/6.02 (Windows NT 4.0; U) [de]',
+    'Opera/6.01 (X11; U; nn)',
+    'Opera/6.01 (Windows XP; U) [de]',
+    'Opera/6.01 (Windows 98; U) [en]',
+    'Opera/6.01 (Windows 98; U) [de]',
+    'Opera/6.01 (Windows 2000; U) [en]',
+    'Opera/6.01 (Windows 2000; U) [de]',
+    'Mozilla/5.0 (Windows 2000; U) Opera 6.01 [en]',
+    'Mozilla/5.0 (Windows 2000; U) Opera 6.01 [de]',
+    'Mozilla/4.78 (Windows 2000; U) Opera 6.01 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows XP) Opera 6.01 [it]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows XP) Opera 6.01 [et]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows XP) Opera 6.01 [de]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows NT 4.0) Opera 6.01 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows NT 4.0) Opera 6.01 [de]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows ME) Opera 6.01 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows ME) Opera 6.01 [de]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98) Opera 6.01 [it]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98) Opera 6.01 [fr]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98) Opera 6.01 [en]',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Windows 98) Opera 6.01 [de]'
+]
+Proxies = [
+    '203.30.191.202	80',
+    '203.13.32.221	80',
+    '203.24.102.56 80',
+    '185.162.230.228 80',
+    '203.23.104.37	80',
+    '45.14.174.140	80',
+    '173.245.49.118	80',
+    '45.12.31.206	80',
+    '172.67.254.148	80',
+    '172.67.181.40	80'
+]
+
+URL = "https://coinmarketcap.com/ru/"
+time.sleep(1)
+fake_user = random.choice(UserAgents)
+proxy = random.choice(Proxies)
+# Проверка соединения
+try:
+    Request = requests.get(URL, headers={'User-Agent': fake_user}, proxies={'Proxy': proxy})
+    if Request.status_code:
+        print(f'Successful connection to {URL}, code {Request.status_code}')
+#    else:
+#       print(f'Error code {Request.status_code}')
+except Exception:
+    Request = requests.get(URL, headers={'User-Agent': random.choice(UserAgents)})
+    if Request.status_code:
+        print('Successful connection!')
+finally:
+    pass
+
+
+# Запись HTML в файл
+# with open('test.html', 'w', encoding='UTF-8') as output_file:
+#    output_file.write(Request.text)
+
+
+a = []
+# Получение HTML страницы
+HTML = BS(Request.text, 'html.parser')
+href = HTML.find('table', class_="h7vnx2-2").find_all('td')
+for td in href:
+    a.append(td.text)
+
+for i in a:
+    if i == '':
+        a.remove(i)
+    print(i, end='\n')
